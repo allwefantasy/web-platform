b29f647d5ceb24c428fde304ee903c9bfdd36a7d
WilliamZhu <allwefantasy@gmail.com>
2022-02-08 17:48:30 +0800

    update

--- src/main/java/tech/mlsql/serviceframework/platform/form/FormParams.scala
+++ src/main/java/tech/mlsql/serviceframework/platform/form/FormParams.scala
@@ -49,35 +49,35 @@ object FormParams {
 
 case class KV(name: Option[String], value: Option[String])
 
-case class CheckBox(name: String, values: List[KV], tpe: String = "CheckBox", @transient valueProvider: Option[() => List[KV]])
+case class CheckBox(name: String, values: List[KV], tpe: String = "CheckBox", @transient valueProvider: Option[() => List[KV]],options:Map[String,String]=Map())
 
-case class Input(name: String, value: String, tpe: String = "Input", @transient valueProvider: Option[() => String] = None)
+case class Input(name: String, value: String, tpe: String = "Input", @transient valueProvider: Option[() => String] = None,options:Map[String,String]=Map())
 
-case class Text(name: String, value: String, tpe: String = "Text", @transient valueProvider: Option[() => String] = None)
+case class Text(name: String, value: String, tpe: String = "Text", @transient valueProvider: Option[() => String] = None,options:Map[String,String]=Map())
 
-case class Select(name: String, values: List[KV], tpe: String = "Select", @transient valueProvider: Option[() => List[KV]] = None)
+case class Select(name: String, values: List[KV], tpe: String = "Select", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class Radio(name: String, values: List[KV], tpe: String = "Radio", @transient valueProvider: Option[() => List[KV]] = None)
+case class Radio(name: String, values: List[KV], tpe: String = "Radio", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class Switch(name: String, values: List[KV], tpe: String = "Switch", @transient valueProvider: Option[() => List[KV]] = None)
+case class Switch(name: String, values: List[KV], tpe: String = "Switch", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class Slider(name: String, values: List[KV], tpe: String = "Slider", @transient valueProvider: Option[() => List[KV]] = None)
+case class Slider(name: String, values: List[KV], tpe: String = "Slider", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class Rate(name: String, values: List[KV], tpe: String = "Rate", @transient valueProvider: Option[() => List[KV]] = None)
+case class Rate(name: String, values: List[KV], tpe: String = "Rate", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class InputNumber(name: String, values: List[KV], tpe: String = "InputNumber", @transient valueProvider: Option[() => List[KV]] = None)
+case class InputNumber(name: String, values: List[KV], tpe: String = "InputNumber", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class TreeSelect(name: String, json: String, tpe: String = "TreeSelect", @transient valueProvider: Option[() => List[KV]] = None)
+case class TreeSelect(name: String, json: String, tpe: String = "TreeSelect", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
 case class Transfer(name: String, sourceValues: List[KV], targetValues: List[KV], tpe: String = "Transfer",
                     @transient sourceValueProvider: Option[() => List[KV]] = None,
-                    @transient targetValueProvider: Option[() => List[KV]] = None)
+                    @transient targetValueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class TimePicker(name: String, values: List[KV], tpe: String = "TimePicker", @transient valueProvider: Option[() => List[KV]] = None)
+case class TimePicker(name: String, values: List[KV], tpe: String = "TimePicker", @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
-case class Upload(name: String, valueProviderName: String, tpe: String = "Upload")
+case class Upload(name: String, valueProviderName: String, tpe: String = "Upload",options:Map[String,String]=Map())
 
-case class Dynamic(name: String, tpe: String = "Dynamic", subTpe: String, depends: List[String], valueProviderName: String)
+case class Dynamic(name: String, tpe: String = "Dynamic", subTpe: String, depends: List[String], valueProviderName: String,options:Map[String,String]=Map())
 
-case class Editor(name: String, tpe: String = "Editor", values: List[KV], @transient valueProvider: Option[() => List[KV]] = None)
+case class Editor(name: String, tpe: String = "Editor", values: List[KV], @transient valueProvider: Option[() => List[KV]] = None,options:Map[String,String]=Map())
 
